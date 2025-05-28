ebf903b53da2964622a5a76b0784959e0c6b0c0d
Ahmed-Yehya84 <ahmedyehya84@gmail.com>
2025-05-28 20:58:43 +0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { getDatabase } from "firebase/database";
+import { getDatabase, enableLogging } from "firebase/database";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
