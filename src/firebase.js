7b4aa42178aa58981a75942e431a9db132911a9c
Ahmed-Yehya84 <ahmedyehya84@gmail.com>
2025-12-29 21:10:30 +0200

    chore: implement environment variables with webpack-dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,13 @@ enableLogging(true);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDKknjtoLV9Icib7Gwf-3fc2936de7f-o",
-  authDomain: "budgetwise-2f8ab.firebaseapp.com",
-  projectId: "budgetwise-2f8ab",
-  storageBucket: "budgetwise-2f8ab.firebasestorage.app",
-  messagingSenderId: "213006654132",
-  appId: "1:213006654132:web:cb45d5c13b8cacc74415e1",
-  databaseURL:
-    "https://budgetwise-2f8ab-default-rtdb.europe-west1.firebasedatabase.app/",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -23,6 +24,8 @@ module.exports = {
       template: "index.html",
       title: "BudgetWise App",
     }),
+
+    new Dotenv(),
   ],
   module: {
     rules: [
